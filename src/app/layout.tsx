6b826698252a73b85fe26243e16a34d6a2b9b832
OnaDebrah <debrahona@gmail.com>
2026-01-08 22:20:09 +0100

    03: database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type {Metadata} from "next";
-import {
-    ClerkProvider,
-    SignInButton,
-    SignUpButton,
-    SignedIn,
-    SignedOut,
-    UserButton,
-} from '@clerk/nextjs'
 import {IBM_Plex_Mono, Inter} from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from "@/components/theme-provider";
-import {Button} from "@/components/ui/button";
-import {dark} from "@clerk/themes";
+import {Providers} from "@/components/providers";
 
 const inter = Inter({
     variable: "--font-inter",
@@ -35,34 +25,15 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <ClerkProvider appearance={{theme: dark}}>
-            <html lang="en" suppressHydrationWarning>
-            <body
-                className={`${inter.variable} ${plexMono.variable} antialiased`}
-            >
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-                disableTransitionOnChange
-            >
-                <header>
-                    <SignedOut>
-                        <SignInButton/>
-                        <SignUpButton>
-                            <Button className="bg-rose-500 text-white p-2 rounded">
-                                Sign Up
-                            </Button>
-                        </SignUpButton>
-                    </SignedOut>
-                    <SignedIn>
-                        <UserButton/>
-                    </SignedIn>
-                </header>
-                {children}
-            </ThemeProvider>
-            </body>
-            </html>
-        </ClerkProvider>
+
+        <html lang="en" suppressHydrationWarning>
+        <body
+            className={`${inter.variable} ${plexMono.variable} antialiased`}
+        >
+        <Providers>
+            {children}
+        </Providers>
+        </body>
+        </html>
     );
 }
