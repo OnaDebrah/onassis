41f386bf65c2af59821dc8ca81428b494da4e713
OnaDebrah <debrahona@gmail.com>
2026-01-09 14:28:39 +0100

    06 Sentry Error tracking

--- src/app/api/demo/blocking/route.ts
+++ src/app/api/demo/blocking/route.ts
@@ -1,13 +1,15 @@
 import {generateText} from "ai";
-import {createGoogleGenerativeAI} from "@ai-sdk/google";
-
-const google = createGoogleGenerativeAI({
-    apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY})
+import {google} from "@ai-sdk/google";
 
 export async function POST() {
     const response = await generateText({
         model: google('gemini-2.5-flash'),
         prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+        experimental_telemetry: {
+            isEnabled: true,
+            recordInputs: true,
+            recordOutputs: true
+        },
     });
 
     return Response.json({response});
--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,9 +1,11 @@
 'use client'
-
+import * as Sentry from '@sentry/nextjs'
 import {Button} from "@/components/ui/button";
 import {useState} from "react";
+import {useAuth} from "@clerk/nextjs";
 
 export default function DemoPage() {
+    const {userId} = useAuth()
     const [loading, setLoading] = useState(false);
     const handleBlocking = async () => {
         setLoading(true);
@@ -17,6 +19,19 @@ export default function DemoPage() {
         setLoading(false);
     };
 
+    const handleClientError = async () => {
+        Sentry.logger.info("User attempting to click on client function", {userId});
+        throw new Error("Client error: Something went wrong in this browser.");
+    }
+
+    const handleApiError = async () => {
+        await fetch("/api/demo/error", {method: "POST"});
+    }
+
+    const handleInngestError = async () => {
+        await fetch("/api/demo/inngest-error", {method: "POST"});
+    }
+
     return (
         <div className="p-8 space-x-4">
             <Button disabled={loading} onClick={handleBlocking}>
@@ -24,6 +39,15 @@ export default function DemoPage() {
             </Button>
             <Button disabled={loading} onClick={handleBackground}>
                 {loading ? "Loading..." : "Background..."}
+            </Button>
+            <Button variant="destructive" onClick={handleClientError}>
+                Client Error
+            </Button>
+            <Button variant="destructive" onClick={handleApiError}>
+                Api Error
+            </Button>
+            <Button variant="destructive" onClick={handleInngestError}>
+                Inngest Error
             </Button>
         </div>
     )
